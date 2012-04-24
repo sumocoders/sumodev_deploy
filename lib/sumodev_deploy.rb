425393d8da70d01fd28528012afbb095e273bab3
Tijs Verkoyen <tijs@verkoyen.eu>
2012-04-24 14:00:36 +0200

    Added sumodev:redirect:put

--- lib/sumodev_deploy.rb
+++ lib/sumodev_deploy.rb
@@ -37,6 +37,32 @@ configuration.load do
         run "info_db #{db_name}"
       end
     end
+    namespace :redirect do
+      desc "Installs the redirect page for the site"
+      task :put, :roles => :app do
+        unless exists?(:production_url)
+          $stderr.puts <<-ERROR
+Define the production_url-variable in your Capfile:
+    set :production_url, \"http://example.com\"
+          ERROR
+          exit 1
+        end
+
+        run %{
+          mkdir -p #{shared_path}/redirect &&
+          wget --quiet -O #{shared_path}/redirect/index.php http://static.sumocoders.be/redirect/index.phps && 
+          wget --quiet -O #{shared_path}/redirect/.htaccess http://static.sumocoders.be/redirect/htaccess
+        }
+
+        # change the url
+        run "if [ -f #{shared_path}/redirect/index.php ]; then sed -i 's/<real-url>/#{production_url}/' #{shared_path}/redirect/index.php; fi"
+
+        run %{
+          rm -f #{current_path} &&
+          ln -s #{shared_path}/redirect #{current_path}
+        }
+      end
+    end
 
     namespace :setup do
       desc "Create the client folder if it doesn't exist yet"
