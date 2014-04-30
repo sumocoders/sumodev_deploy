eff06a1c7aed406a54df527f313aefa494ac2abb
Jan De Poorter <jan@defv.be>
2014-04-30 14:58:58 +0200

    Check if a redirect page is installed, if it is prompt the user

--- lib/sumodev_deploy/tasks/redirect.rb
+++ lib/sumodev_deploy/tasks/redirect.rb
@@ -1,6 +1,17 @@
 Capistrano::Configuration.instance.load do
   namespace :sumodev do
     namespace :redirect do
+      task :check do
+        current_symlink = capture("readlink -f #{current_path}").chomp
+        if current_symlink == "#{shared_path}/redirect" # Project has redirect set up
+          sure = Capistrano::CLI.ui.ask "This application has a redirect page installed. Are you sure you want to override this? (y)es/(n)o"
+
+          if sure !~ /^y(es)?/
+            exit 1
+          end
+        end
+      end
+
       desc "Installs the redirect page for the site"
       task :put, :roles => :app do
         unless exists?(:production_url)
@@ -25,4 +36,6 @@ Capistrano::Configuration.instance.load do
       end
     end
   end
+
+  before 'deploy', 'sumodev:redirect:check'
 end
