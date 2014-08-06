f0fa28adc5093ad9add872ebd5efa44565e7ddf9
Jan De Poorter <jan@defv.be>
2014-08-06 17:57:05 +0200

    Detect newly created directories, create them on the server as well

--- lib/sumodev_deploy/tasks/assets.rb
+++ lib/sumodev_deploy/tasks/assets.rb
@@ -15,11 +15,15 @@ Capistrano::Configuration.instance.load do
         run_locally "rm -rf #{asset_path} && mkdir -p #{asset_path}" # Cleanup
         run_locally "cd #{path} && compass clean --css-dir #{asset_path}/#{Compass.configuration.css_dir} && compass compile -s #{output_style} --css-dir #{asset_path}/#{Compass.configuration.css_dir}"
 
-        assets  = Dir.glob(asset_path + '/*/*').map {|f| [f, f.gsub(asset_path, '')] }
+        assets  = Dir.glob(asset_path + '/**/*').map {|f| [f, f.gsub(asset_path, '')] }
         sprites = Dir.glob(path + "/images/**/*").grep(/-[0-9a-z]{11}.(png|jpg)/).map {|f| [f, f.gsub(path, '')] }
 
         (assets + sprites).each do |file, filepath|
-          upload file, "#{latest_release.shellescape}/#{path}#{filepath}"
+          if File.directory?(file)
+            run "mkdir -p #{latest_release.shellescape}/#{path}/#{filepath}"
+          else
+            upload file, "#{latest_release.shellescape}/#{path}#{filepath}"
+          end
         end
 
         run_locally "rm -rf #{asset_path}"
