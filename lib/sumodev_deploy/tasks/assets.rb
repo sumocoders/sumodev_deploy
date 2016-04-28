e5a1e0a10b65cc5c4f8e628b0b427bf9ce3629a5
Wouter Sioen <wouter.sioen@gmail.com>
2016-04-28 10:02:22 +0200

    Allow us to use gulp instead of grunt

--- lib/sumodev_deploy/tasks/assets.rb
+++ lib/sumodev_deploy/tasks/assets.rb
@@ -2,15 +2,22 @@ Capistrano::Configuration.instance.load do
   namespace :assets do
     desc "Compile and upload the assets"
     task :precompile do
-      if !File.exist?("Gruntfile.coffee")
-        logger.important "No Gruntfile.coffee found"
-      else
+      if File.exist?("Gruntfile.coffee")
         run_locally "grunt build"
         run %{
           rm -rf #{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core &&
           mkdir -p #{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core
         }
         upload "./src/Frontend/Themes/#{theme}/Core", "#{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core"
+      else if File.exist?("gulpfile.js")
+        run_locally "gulp build"
+        run %{
+          rm -rf #{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core &&
+          mkdir -p #{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core
+        }
+        upload "./src/Frontend/Themes/#{theme}/Core", "#{latest_release.shellescape}/src/Frontend/Themes/#{theme}/Core"
+      else
+        logger.important "No Gruntfile.coffee or gulpfile.js found"
       end
     end
   end
