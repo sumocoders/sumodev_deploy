819b2666774e0c5b9590120143c07e271de0d191
Tijs Verkoyen <tijs@verkoyen.eu>
2017-04-26 17:09:34 +0200

    Added a way to reset the opcache thru a file

--- lib/sumodev_deploy/tasks/opcache.rb
+++ lib/sumodev_deploy/tasks/opcache.rb
@@ -1,4 +1,15 @@
 Capistrano::Configuration.instance.load do
   namespace :opcache do
+    namespace :file do
+      desc "Reset the opcache thru a PHP-file"
+      task :reset do
+        run %{
+            touch #{current_path}/php-opcache-reset.php
+            && echo "<?php clearstatcache(true); if (function_exists('opcache_reset')) { opcache_reset(); }" > #{current_path}/php-opcache-reset.php
+            && curl -L --fail --silent --show-error "#{site_url}/php-opcache-reset.php"
+            && rm #{current_path}/php-opcache-reset.php
+        }
+      end
+    end
   end
 end
